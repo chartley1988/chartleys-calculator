044e886df51ec7c08c9056f8ac70b16c887c78a6
Carson Hartley <chartley1988@gmail.com>
2023-02-05 15:31:14 -0700

    Shunting Yard - Works well, but need to add more symbol compatibility

--- src/tests/ShuntingYard.test.ts
+++ src/tests/ShuntingYard.test.ts
@@ -1,10 +1,12 @@
 import { test, expect } from 'vitest';
 
-function splitString (inputString: string): string[] {
-	const splitTokens = inputString.split(/[\s]|(?=[\^*/+-])|(?<=[\^*/+-])|(?<=[\\(\\)])|(?=[\\(\\)])/g);
-		const removeEmpties = splitTokens.filter(entry => entry !== "");
-		return removeEmpties;
-	}
+function splitString(inputString: string): string[] {
+	const splitTokens = inputString.split(
+		/[\s]|(?=[\^*/+-])|(?<=[\^*/+-])|(?<=[\\(\\)])|(?=[\\(\\)])/g
+	);
+	const removeEmpties = splitTokens.filter((entry) => entry !== '');
+	return removeEmpties;
+}
 
 /*
 PSEUDO CODE
@@ -13,24 +15,9 @@ Output  =   [] (Represents finished Reverse Polish Notation)
 Stack   =   [] (Represents a list of operators, a staging area)
 Tokens  =   [] (Input data)
 
-
-1) If TOKEN is a number (operand), add to the OUTPUT (print it)
-
-2) If TOKEN is a left-bracket, push it to the STACK
-
-3) If TOKEN is a right-bracket, discard the right-bracket and pop the TOKENS from the STACK to the OUTPUT until the left-bracket is found. Pop and discard the left bracket.
-
-4) If the TOKEN is an operator and the stack is empty or contains a left bracket on top, push the operator onto the stack.
-
-5) If the TOKEN is an operator and has either higher precedence than the operator on the top of the STACK, or has the same precedence as the operator on top of the stack and is right-associative -- push it on to the STACK
-
-6) If the TOKEN is an operator and has either lower precedence than the operator on the top of the STACK, or has the same precedence as the operator on the top of the STACK and is left associative -- continue to pop the STACK until this is not true. Then, push the incoming operator to the STACK.
-
-7) At the end of the expression, pop all remaining operators from STACK to OUTPUT. No brackets should remain.
-
 */
 
-const testTokens = splitString('1 + 3 * 9 / (2+4)')
+const testTokens = splitString('4+18/(9-3)');
 
 function parseInfix(tokens) {
 	const input: string[] = tokens.map((x) => x);
@@ -43,79 +30,66 @@ function parseInfix(tokens) {
 
 	for (let i = 0; i < tokens.length; i++) {
 		const token: string = tokens[i];
+		console.log(`CURRENT TOKEN: ${token}`)
+
+		// If it's a number, add it to the output
 		if (isOperand(token)) {
 			output.push(token);
 			logResults(stack, output);
 			continue;
 		}
+
+		// If it's an operator..
+		if (isOperator(token)) {
+			// While there's an operator on top of the stack with greater precedence
+			if (stack.length !== 0) {
+				console.log(getPrecedence(stack[stack.length - 1]));
+				console.log(getPrecedence(token));
+				while (
+					(stack[stack.length - 1] !== '(' &&
+						getPrecedence(stack[stack.length - 1]) -
+							getPrecedence(token) >
+							0) ||
+					(getPrecedence(stack[stack.length - 1]) -
+						getPrecedence(token) ===
+						0 &&
+						isLeftAssociative(token))
+				) {
+					// Pop operators from the stack to output
+					const movedToken = stack.pop() as string;
+					output.push(movedToken);
+				}
+			}
+			stack.push(token);
+			logResults(stack, output);
+			continue;
+		}
+
+		// If it's a left bracket, push it to the stack
 		if (isLeftBracket(token)) {
 			stack.push(token);
 			logResults(stack, output);
 			continue;
 		}
+
+		// If it's a right bracket...
 		if (isRightBracket(token)) {
-			while (stack.length > 0) {
-				const topStackToken = stack[stack.length - 1];
-				if (isLeftBracket(topStackToken)) {
-					stack.pop();
-					logResults(stack, output);
-					break;
-				} else {
-					stack.pop();
-					output.push(topStackToken);
-					logResults(stack, output);
-					continue;
-				}
+			// While there's not a left bracket at the top of the stack:
+			while (stack[stack.length - 1] !== '(') {
+				// Pop operators from stack on to output
+				const movedToken = stack.pop() as string;
+				output.push(movedToken);
 			}
-			continue;
-		}
-		
-		if (isOperator(token) && (stack.length === 0 || stack[0] === '(')) {
-			stack.push(token);
-			logResults(stack, output);
-			continue;
-		}
-		
-		if (isOperator(token)) {
-			if (
-				getPrecedence(token) > getPrecedence(stack[stack.length - 1]) ||
-				(getPrecedence(token) ===
-				getPrecedence(stack[stack.length - 1]) &&
-				isRightAssociative(token))
-				) {
-					stack.push(token);
-				}
-				logResults(stack, output);
-				console.log("I'm here!")
-				continue;
-			}
-			
-		if (isOperator(token)) {
-			if (
-				getPrecedence(token) <=
-				getPrecedence(stack[stack.length - 1]) &&
-				isLeftAssociative(token)
-				) {
-					while (
-						getPrecedence(token) <=
-						getPrecedence(stack[stack.length - 1]) &&
-						isLeftAssociative(token)
-						) {
-							let removedToken = stack.pop() as string;
-							output.push(removedToken);
-							logResults(stack, output);
-						}
-						stack.push(token);
-					}
-					logResults(stack, output);
-			continue;
-		}
-		
-		stack.forEach(token => {
+			// Pop the left bracket and discard it
 			stack.pop();
-			output.push(token);
 			logResults(stack, output);
-		});
+		}
+	}
+	while (stack.length > 0) {
+		// Pop operators from stack on to output
+		const movedToken = stack.pop() as string;
+		output.push(movedToken);
+		logResults(stack, output);
 	}
 }
 
@@ -196,5 +170,5 @@ describe('Shunting Yard - Parses infix to postfix', () => {
 
 	test('Is left?', () => {
 		expect(isLeftAssociative('++')).toBe(true);
-	})
+	});
 });
