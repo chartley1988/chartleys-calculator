daf2e5f42b654eb2e2be1a8d6cd11a1db419adc9
Carson Hartley <chartley1988@gmail.com>
2023-02-12 13:30:22 -0700

    Added Line re-ordering

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -5,8 +5,11 @@ import splitString from '../math/string_parse';
 import shuntingYard from '../math/shunting_yard';
 import operateRPN from '../math/operate_RPN';
 import Footer from './Footer';
+import FooterSelected from './FooterSelected';
 
 function Editor() {
+
+
 	const [data, setData] = useState([
 		{
 			line_number: 1,
@@ -28,6 +31,9 @@ function Editor() {
 		},
 	]);
 
+	const [lineSelected, setLineSelected] = useState(false);
+	const [currentLine, setCurrentLine] = useState(0);
+
 	function calculateResult(input, line_number) {
 		const updatedData = data.slice();
 		const tokens = splitString(input);
@@ -36,6 +42,7 @@ function Editor() {
 		const entry = updatedData.find(
 			(entry) => entry.line_number === line_number
 		);
+		entry.input_string = input;
 		entry.output_string = result;
 		setData(updatedData);
 	}
@@ -51,16 +58,120 @@ function Editor() {
 				entry.selected = false;
 			});
 			entry.selected = true;
+			setLineSelected(true);
+			setCurrentLine(line_number);
 		} else {
 			updatedData.forEach((entry) => {
 				entry.selected = false;
 			});
 			entry.selected = false;
+			setLineSelected(false);
+			setCurrentLine(0);
 		}
 		setData(updatedData);
 	}
 
 	function clearLineSelection() {}
+
+	function renderFooter() {
+		if (lineSelected) {
+			return (
+				<FooterSelected
+					moveLineUp={moveLineUp}
+					moveLineDown={moveLineDown}
+					currentLine={currentLine}
+				/>
+			);
+		} else {
+			return <Footer clearSheet={clearSheet} addLine={addLine} />;
+		}
+	}
+
+	function clearSheet() {
+		setData([
+			{
+				line_number: 1,
+				input_string: '',
+				output_string: '',
+				selected: false,
+			},
+		]);
+	}
+
+	function addLine() {
+		const updatedData = data.slice();
+		const newLine = {
+			line_number: data.length + 1,
+			input_string: '',
+			output_string: '',
+			selected: false,
+		};
+		updatedData.push(newLine);
+		setData(updatedData);
+	}
+
+	function moveLineUp(line_number) {
+		if(line_number > 1) {
+			const updatedData = data.slice();
+			const entry = updatedData.find(
+				(entry) => entry.line_number === line_number
+			);
+			const index = updatedData.indexOf(entry);
+			const deleted = updatedData.splice(index, 1);
+			updatedData.forEach(entry => {
+				entry.selected = false
+			})
+			updatedData.splice((index-1), 0, {
+				line_number: 1, // Temp, reset below
+				input_string: deleted[0].input_string,
+				output_string: deleted[0].output_string,
+				selected: true,
+			});
+			setCurrentLine((index));
+			updatedData.forEach(entry => {
+				entry.line_number = (updatedData.indexOf(entry)+1)
+			})
+			setData(updatedData);
+			updateLineInputs(updatedData);
+		}
+	}
+
+	function moveLineDown(line_number) {
+		if(line_number < data.length) {
+			const updatedData = data.slice();
+			const entry = updatedData.find(
+				(entry) => entry.line_number === line_number
+			);
+			const index = updatedData.indexOf(entry);
+			const deleted = updatedData.splice(index, 1);
+			updatedData.forEach(entry => {
+				entry.selected = false
+			})
+			updatedData.splice((index+1), 0, {
+				line_number: 1, // Temp, reset below
+				input_string: deleted[0].input_string,
+				output_string: deleted[0].output_string,
+				selected: true,
+			});
+			setCurrentLine((index+2));
+			updatedData.forEach(entry => {
+				entry.line_number = (updatedData.indexOf(entry)+1)
+			})
+			setData(updatedData);
+			updateLineInputs(updatedData);
+		}
+	}
+
+	function updateLineInputs(input_data) {
+		//console.log(input_data)
+		const inputs = document.getElementsByClassName('editor-input-line');
+		const inputsArray = Array.from(inputs);
+		inputsArray.forEach(entry => {
+			const index = inputsArray.indexOf(entry);
+			//console.log(input_data[index])
+			entry.value = input_data[index].input_string
+		})
+	}
 
 	return (
 		<div onClick={clearLineSelection}>
@@ -78,7 +189,7 @@ function Editor() {
 					);
 				})}
 			</ul>
-			<Footer />
+			{renderFooter()}
 		</div>
 	);
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,42 +1,24 @@
-function Footer() {
+import '../css/footer.css'
+
+function Footer(props) {
+   const { clearSheet, addLine } = props
+
 	return (
 		<footer>
-			<button>Clear Sheet</button>
-			<button>Add Line</button>
+            <div style={{
+                display:'flex',
+                gap: '0.75rem'
+            }}>
+                <button className='footer-button desktop' onClick={clearSheet} >Clear Sheet</button>
 
-			<button>
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					fill='none'
-					viewBox='0 0 24 24'
-					strokeWidth={1.5}
-					stroke='currentColor'
-					className='w-6 h-6'
-				>
-					<path
-						strokeLinecap='round'
-						strokeLinejoin='round'
-						d='M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5'
-					/>
-				</svg>
-			</button>
+                <button className='footer-button mobile' onClick={clearSheet} >Clr</button>
 
-			<button>
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					fill='none'
-					viewBox='0 0 24 24'
-					strokeWidth={1.5}
-					stroke='currentColor'
-					className='w-6 h-6'
-				>
-					<path
-						strokeLinecap='round'
-						strokeLinejoin='round'
-						d='M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5'
-					/>
-				</svg>
-			</button>
+                <button className='footer-button desktop' onClick={addLine} >Add Line</button>
+
+                <button className='footer-button mobile' onClick={addLine} >+</button>
+
+            </div>
+            <p id="editor-sum">{`Sum: 42`}</p>
 		</footer>
 	);
 }
