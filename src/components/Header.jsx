d8a797d10bab9dbe26797a1d22e9eab52cfe0826
Carson Hartley <chartley1988@gmail.com>
2023-05-10 09:52:26 -0600

    Change text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ function SignIn() {
 		<div style={{ display: 'flex', justifyContent: 'center' }}>
 			<h2>{user ? `${user}` : ''}</h2>
 			<button onClick={user ? userContext.signOutUser : userContext.signIn}>
-				{user ? 'SignOut' : 'Sign In'}
+				{user ? 'Sign Out' : 'Sign In'}
 			</button>
 		</div>
 	);
