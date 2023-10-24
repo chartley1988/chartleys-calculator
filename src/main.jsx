eb636f9aee8c8ff01b7500b78b76b39267bf15e7
Carson Hartley <chartley1988@gmail.com>
2023-10-24 17:49:20 -0600

    Installed React router, made blank home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Home from './HomePage/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/calculator",
+    element: <App />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
